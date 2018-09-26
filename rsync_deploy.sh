e11af41855066b456d69662718bb45c304cff31f
Jonah Brüchert <jbb.prv@gmx.de>
2018-09-26 20:10:27 +0200

    Fix rsync progress

--- rsync_deploy.sh
+++ rsync_deploy.sh
@@ -4,20 +4,7 @@ echo $DEPLOY_KEY_PRIVATE | base64 -d | xz -d > ~/.ssh/id_rsa
 echo $DEPLOY_KEY_PUBLIC | base64 -d | xz -d > ~/.ssh/id_rsa.pub
 chmod 400 ~/.ssh/id_rsa
 
-rsync -avzp -e \
+rsync -avzP -e \
         "ssh -o StrictHostKeyChecking=no -p $DEPLOY_PORT" \
         $TRAVIS_BUILD_DIR/debian-*.tar.xz \
         $DEPLOY_ACCOUNT:/home/travis/ &
-pid=$!
-
-# If this script is killed, kill the "rsync".
-trap "kill $pid 2> /dev/null" EXIT
-
-# While rsync is running...
-while kill -0 $pid 2> /dev/null; do
-	echo -n "."
-	sleep 1
-done
-
-# Disable the trap on a normal exit.
-trap - EXIT
