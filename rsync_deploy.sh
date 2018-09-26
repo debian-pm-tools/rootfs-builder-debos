7d7ba54d3c0dc68247a786a9f0004ade54ddb6bc
Jonah Brüchert <jbb.prv@gmx.de>
2018-09-26 16:45:42 +0200

    Deploy using rsync

---
+++ rsync_deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo $DEPLOY_KEY_PRIVATE | base64 -d | xz -d > ~/.ssh/id_rsa
+echo $DEPLOY_KEY_PUBLIC | base64 -d | xz -d > ~/.ssh/id_rsa.pub
+chmod 400 ~/.ssh/id_rsa
+
+rsync -avzp -e "ssh -o StrictHostKeyChecking=no -p $DEPLOY_PORT" --quiet $TRAVIS_BUILD_DIR/debian-*.tar.xz $DEPLOY_ACCOUNT:/home/travis/
