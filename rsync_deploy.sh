db672898f7e3b586da43d8d578ac73ea62dd5bef
Jonah Brüchert <jbb.prv@gmx.de>
2018-09-26 19:23:36 +0200

    deploy: Don't deploy quietly

--- rsync_deploy.sh
+++ rsync_deploy.sh
@@ -4,4 +4,4 @@ echo $DEPLOY_KEY_PRIVATE | base64 -d | xz -d > ~/.ssh/id_rsa
 echo $DEPLOY_KEY_PUBLIC | base64 -d | xz -d > ~/.ssh/id_rsa.pub
 chmod 400 ~/.ssh/id_rsa
 
-rsync -avzp -e "ssh -o StrictHostKeyChecking=no -p $DEPLOY_PORT" --quiet $TRAVIS_BUILD_DIR/debian-*.tar.xz $DEPLOY_ACCOUNT:/home/travis/
+rsync -avzp -e "ssh -o StrictHostKeyChecking=no -p $DEPLOY_PORT" $TRAVIS_BUILD_DIR/debian-*.tar.xz $DEPLOY_ACCOUNT:/home/travis/
