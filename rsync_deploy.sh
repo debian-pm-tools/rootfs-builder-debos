963357e8ea7b2236d17c820eac9047fdfd7d39b8
Jonah Brüchert <jbb.prv@gmx.de>
2018-09-30 10:48:07 +0200

    deploy: Generate checksums

--- rsync_deploy.sh
+++ rsync_deploy.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+FILES="debian-*.tar.xz*"
+
 echo $DEPLOY_KEY_PRIVATE | base64 -d | xz -d > ~/.ssh/id_rsa
 echo $DEPLOY_KEY_PUBLIC | base64 -d | xz -d > ~/.ssh/id_rsa.pub
 chmod 400 ~/.ssh/id_rsa
 
+# Generate checksums
+sha256sum $FILES > sha256sums
+
+# Deploy to server
 rsync -avzP -e \
         "ssh -o StrictHostKeyChecking=no -p $DEPLOY_PORT" \
-        $TRAVIS_BUILD_DIR/debian-*.tar.xz \
+        $TRAVIS_BUILD_DIR/$FILES \
         $DEPLOY_ACCOUNT:/var/www/archive.kaidan.im/debian-pm
