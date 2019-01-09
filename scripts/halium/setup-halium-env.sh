76ed9b6b54b930dbe1fee4ea857ea085e111501b
Jonah Brüchert <jbb.prv@gmx.de>
2019-01-09 19:22:54 +0100

    Clean up environment setup

--- scripts/halium/setup-halium-env.sh
+++ scripts/halium/setup-halium-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "I: setting up initial environment for Touch..."
+echo "I: setting up initial environment for Halium..."
 
 echo "HOSTNAME=android
 TERM=linux
@@ -11,15 +11,11 @@ ANDROID_PROPERTY_WORKSPACE=8,49152
 ANDROID_ASSETS=/system/app
 ANDROID_BOOTLOGO=1
 LD_LIBRARY_PATH=/vendor/lib:/system/lib
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 ANDROID_DATA=/data
-QML2_IMPORT_PATH=/usr/lib/x86_64-linux-gnu/qt5/imports
-QT_SELECT=qt5
 SHLVL=1
 MKSH=/system/bin/sh
 ANDROID_ROOT=/system
 EXTERNAL_STORAGE=/mnt/sdcard
 LANG=en_US.UTF-8
 LANGUAGE=en_US:en
-QT_VIRTUALKEYBOARD_STYLE=Plasma
 FLASH_KERNEL_SKIP=true" > /etc/environment
--- scripts/setup-plasma-env.sh
+++ scripts/setup-plasma-env.sh
@@ -1,15 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "I: adding plasma-phone related env vars"
 
 echo QT_QPA_PLATFORM=wayland >> /etc/environment
 echo FORCE_RIL_NUM_MODEMS=1 >> /etc/environment
-sed s,QT_IM_MODULE.*,, /etc/environment -i
-sed s,GTK_IM_MODULE.*,, /etc/environment -i
 echo GTK_IM_MODULE=maliit >> /etc/environment
-
-sed s,PATH.*,, /etc/environment -i
 echo PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games >> /etc/environment
 
 echo GDK_BACKEND=x11 >> /etc/environment
 echo QT_GSTREAMER_CAMERABIN_SRC=droidcamsrc >> /etc/environment
+
+echo QT_SELECT=qt5 >> /etc/environment
+echo QT_VIRTUALKEYBOARD_STYLE=Plasma >> /etc/environment
