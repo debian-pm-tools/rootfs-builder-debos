0b6a088242e0dcd908788da4057956a3f54dc519
Jonah Brüchert <jbb.prv@gmx.de>
2019-01-21 18:53:21 +0100

    Add basic devuan support

--- scripts/halium/enable-services.sh
+++ scripts/halium/enable-services.sh
@@ -1,12 +1,16 @@
 #!/bin/sh -x
 
-systemctl enable system.mount
-systemctl enable android-mount.service
+if [ command -v systemctl >/dev/null ]; then
+    systemctl enable system.mount
+    systemctl enable android-mount.service
 
-#systemctl enable adbd.service
-systemctl enable usb-tethering
-systemctl enable dropbear
+    #systemctl enable adbd.service
+    systemctl enable usb-tethering
+    systemctl enable dropbear
 
-systemctl enable lxc@android.service
+    systemctl enable lxc@android.service
 
-systemctl set-default multi-user.target
+    systemctl set-default multi-user.target
+else
+    # To be implemented
+fi
