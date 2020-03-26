b9d27ee84f7f04359b10b24ecbb2775e1d895f50
Jonah Brüchert <jbb.prv@gmx.de>
2020-03-27 00:29:21 +0100

    Try to fix deployment

--- rsync_deploy.sh
+++ rsync_deploy.sh
@@ -6,12 +6,6 @@ echo $DEPLOY_KEY_PRIVATE | base64 -d | xz -d > ~/.ssh/id_rsa
 echo $DEPLOY_KEY_PUBLIC | base64 -d | xz -d > ~/.ssh/id_rsa.pub
 chmod 400 ~/.ssh/id_rsa
 
-# Generate checksums and zsyncfiles
-for file in $FILES; do
-	sha256sum $file > ${file}.sha256
-	zsyncmake -C -b 2048 -u ${file} ${file}
-done
-
 # Deploy to server
 rsync -avzP -e \
         "ssh -o StrictHostKeyChecking=no -p $DEPLOY_PORT" \
