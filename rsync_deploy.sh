17d1e7e6bbc74f440215a60b6fb1086cae2c0e62
Jonah Brüchert <jbb.prv@gmx.de>
2020-03-27 01:03:36 +0100

    deploy: Add -A --no-perms

--- rsync_deploy.sh
+++ rsync_deploy.sh
@@ -7,7 +7,7 @@ echo $DEPLOY_KEY_PUBLIC | base64 -d | xz -d > ~/.ssh/id_rsa.pub
 chmod 400 ~/.ssh/id_rsa
 
 # Deploy to server
-rsync -avzP -e \
+rsync -A --no-perms -avzP -e \
         "ssh -o StrictHostKeyChecking=no -p $DEPLOY_PORT" \
         $TRAVIS_BUILD_DIR/$FILES \
         $DEPLOY_ACCOUNT:/var/www/archive.kaidan.im/debian-pm/images/$RECIPE/$VARIANT/
